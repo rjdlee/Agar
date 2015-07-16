d0eca72cef388cbce8f61cce3be3f58e33b2c717
Richard Lee <rjdlee@gmail.com>
2015-07-16 13:40:03 -0400

    Port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require( 'express' ),
 	app = express(),
-	server = app.listen( 2222 ),
+	server = app.listen( 8080, '127.0.0.1' ),
 	io = require( 'socket.io' )( server ),
 	fs = require( 'fs' ),
 	path = require( 'path' ),
